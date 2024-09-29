c94104dc88b4eee4f3d89b5fb43870766f335f75
Abdurrahmans <abdurrahmansmark19972gmail.com>
2024-09-29 12:06:05 +0600

    Router configer

---
+++ order_routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+
+order_router = APIRouter(
+    prefix="/orders",
+    tags=['order']
+)
+
+@order_router.get('/')
+async def hello():
+    return{'message':'hello world oreder'}
